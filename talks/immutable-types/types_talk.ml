c835b8eee3e23e918cd7dd7e8c99541d7495fad5
James Geddes <jgeddes@turing.ac.uk>
2025-11-18 15:06:23 +0000

    Version as presented

--- talks/immutable-types/types_talk.ml
+++ talks/immutable-types/types_talk.ml
@@ -16,6 +16,27 @@ open Int (* Pay no attention to the man behind the curtain *)
      -----
   *)
 
+(* Program 1 *)
+
+(* let add2 x = *)
+(*   succ (succ x) *)
+
+(* Program 2 *)
+
+let add2 = (add 2)
+
+type port =
+  Port of int
+
+type string_option  =
+  | None
+  | Some of string
+
+let name_of_port p =
+  match p with
+  | Port 20 -> Some "FTP"
+  | Port 80 -> Some "HTTP"
+  | Port _       -> None
 
 
 
@@ -23,10 +44,18 @@ open Int (* Pay no attention to the man behind the curtain *)
      ---------------------
  *)
 
+type 'a list =
+  | Nil
+  | Cons of ('a * 'a list)
 
+(* Program 3 *)
 
+let rec fold f init xs =
+  match xs with
+  | Nil -> init
+  | Cons (x, rest) -> f x (fold f init rest)
 
-
+let sum = fold add 0
 
 
 
